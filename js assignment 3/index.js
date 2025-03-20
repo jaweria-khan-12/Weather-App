02afa8e8881a5388cc1acb4ab967f5b2663f379f
jaweria-khan-12 <jaweriya0981234@gmail.com>
2025-03-20 20:20:56 +0500

    Add files via upload

---
+++ js assignment 3/index.js
@@ -0,0 +1,230 @@
+let userinput = prompt("Enter Your City Name:");
+
+if (userinput == "sydney") {
+
+    document.write('<div class="main">' +
+  
+                '<div class="container">' +
+                    '<div class="box-1">' +
+                       '<div class="img-1">' + 
+                           '<img class="sydney" src="./857a92e7-a85b-4635-8903-8b6a33b2ba59.webp">' +
+                           '<h1 class="sydney-text"> Sydney </h1>' +
+                           '<h3 class="sydney-text-1">74 F</h3>' +
+                           '<h4 class="sydney-text-2">Mostly Sunny</h4>' +
+                       '</div>' +
+                           '<img class="sydney-logo" src="./Sun-PNG-Pic.png" alt="">' +
+                    '</div>' +
+                   
+                '</div>' +
+       
+                '<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-1">' +
+                   '<div class="container-fluid">' +
+                     '<a class="navbar-brand nav-heading" href="#">Weather App</a>' +
+                     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+                       '<span class="navbar-toggler-icon"></span>' +
+                     '</button>' +
+                     '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+                       '<ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">KARACHI</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">SYDNEY</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">BRAZIL</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">INDONESIA</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">ANTARCTICA</a>' +
+                         '</li>' +
+                     '</div>' +
+                   '</div>' +
+                 '</nav>' +
+            '</div>');
+
+}else if(userinput == "brazil"){
+
+    document.write('<div class="main">' +
+  
+                '<div class="container">' +
+                    '<div class="box-1">' +
+                       '<div class="img-1">' + 
+                           '<img class="brazil" src="./7508a572-7f8e-49a1-a69f-00ff5c7720d2.webp">' +
+                           '<h1 class="brazil-text"> Brazil </h1>' +
+                           '<h3 class="brazil-text-1">73 F</h3>' +
+                           '<h4 class="brazil-text-2">Mostly Rainy</h4>' +
+                       '</div>' +
+                           '<img class="brazil-logo" src="./cloud-rain-icon-2.png" alt="">' +
+                    '</div>' +
+                   
+                '</div>' +
+       
+                '<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-1">' +
+                   '<div class="container-fluid">' +
+                     '<a class="navbar-brand nav-heading" href="#">Weather App</a>' +
+                     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+                       '<span class="navbar-toggler-icon"></span>' +
+                     '</button>' +
+                     '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+                       '<ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">KARACHI</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">SYDNEY</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">BRAZIL</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">INDONESIA</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">ANTARCTICA</a>' +
+                         '</li>' +
+                     '</div>' +
+                   '</div>' +
+                 '</nav>' +
+            '</div>');
+
+}else if(userinput == "karachi"){
+
+    document.write('<div class="main">' +
+  
+         '<div class="container">' +
+             '<div class="box-1">' +
+                '<div class="img-1">' +
+                    '<img class="karachi" src="./cbfda82e-c5fb-4211-ba25-adae1d5ff325.webp">' +
+                    '<h1 class="karachi-text"> Karachi </h1>' +
+                    '<h3 class="karachi-text-1">34 C</h3>' +
+                    '<h4 class="karachi-text-2">Mostly Sunny</h4>' +
+                '</div>' +
+                    '<img class="karachi-logo" src="./Sun-PNG-Pic.png">' +
+             '</div>' +
+            
+         '</div>' +
+
+         '<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-1">' +
+            '<div class="container-fluid">' +
+              '<a class="navbar-brand nav-heading" href="#">Weather App</a>' +
+              '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+                '<span class="navbar-toggler-icon"></span>' +
+              '</button>' +
+              '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+                '<ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
+                  '<li class="nav-item">' +
+                    '<a class="nav-link active ms-5" aria-current="page" href="#">KARACHI</a>' +
+                  '</li>' +
+                  '<li class="nav-item">' +
+                    '<a class="nav-link active ms-5" aria-current="page" href="#">SYDNEY</a>' +
+                  '</li>' +
+                  '<li class="nav-item">' +
+                    '<a class="nav-link active ms-5" aria-current="page" href="#">BRAZIL</a>' +
+                  '</li>' +
+                  '<li class="nav-item">' +
+                    '<a class="nav-link active ms-5" aria-current="page" href="#">INDONESIA</a>' +
+                  '</li>' +
+                  '<li class="nav-item">' +
+                    '<a class="nav-link active ms-5" aria-current="page" href="#">ANTARCTICA</a>' +
+                  '</li>' +
+              '</div>' +
+            '</div>' +
+          '</nav>' +
+     '</div>');
+
+}else if(userinput == "antarctica"){
+
+    document.write('<div class="main">' +
+  
+                '<div class="container">' +
+                    '<div class="box-1">' +
+                       '<div class="img-1">' + 
+                           '<img class="antarctica" src="./446b017e-d323-4842-bb12-7c2637f492ab.webp">' +
+                           '<h1 class="antarctica-text"> Antarctica </h1>' +
+                           '<h3 class="antarctica-text-1">-5 C</h3>' +
+                           '<h4 class="antarctica-text-2">Mostly Winter</h4>' +
+                       '</div>' +
+                           '<img class="antarctica-logo" src="./cloud-with-snow-emoji-by-google.png">' +
+                    '</div>' +
+                   
+                '</div>' +
+       
+                '<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-1">' +
+                   '<div class="container-fluid">' +
+                     '<a class="navbar-brand nav-heading" href="#">Weather App</a>' +
+                     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+                       '<span class="navbar-toggler-icon"></span>' +
+                     '</button>' +
+                     '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+                       '<ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">KARACHI</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">SYDNEY</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">BRAZIL</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">INDONESIA</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">ANTARCTICA</a>' +
+                         '</li>' +
+                     '</div>' +
+                   '</div>' +
+                 '</nav>' +
+            '</div>');
+
+}else if(userinput == "indonesia"){
+
+    document.write('<div class="main">' +
+  
+                '<div class="container">' +
+                    '<div class="box-1">' +
+                       '<div class="img-1">' + 
+                           '<img class="indonesia" src="./97a4a737-2522-4ba9-b270-d878ed4602a7.webp">' +
+                           '<h1 class="indonesia-text"> Indonesia </h1>' +
+                           '<h3 class="indonesia-text-1">84 F</h3>' +
+                           '<h4 class="indonesia-text-2">Mostly Humid</h4>' +
+                       '</div>' +
+                           '<img class="indonesia-logo" src="./9c5c344bd55f42acf78967c081230a61.png" alt="">' +
+                    '</div>' +
+                   
+                '</div>' +
+       
+                '<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-1">' +
+                   '<div class="container-fluid">' +
+                     '<a class="navbar-brand nav-heading" href="#">Weather App</a>' +
+                     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+                       '<span class="navbar-toggler-icon"></span>' +
+                     '</button>' +
+                     '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+                       '<ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">KARACHI</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">SYDNEY</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">BRAZIL</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">INDONESIA</a>' +
+                         '</li>' +
+                         '<li class="nav-item">' +
+                           '<a class="nav-link active ms-5" aria-current="page" href="#">ANTARCTICA</a>' +
+                         '</li>' +
+                     '</div>' +
+                   '</div>' +
+                 '</nav>' +
+            '</div>');
+
+}else{
+    document.write('<img style= "width: 700px; height: 700px; margin-top: -40px; margin-left: 330px; overflow-y: hidden;" src="./Sorry-Emoji-PNG-Image-Background.png">');
+}
